df0be10dfa373dcf9ff861d0bb24a695aff6b40a
Pierluigi Viti <pierluigi@commercelayer.io>
2025-04-14 17:35:12 +0200

    chore: restore library compatibility with sdk7 only

--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,4 @@
-import { ApiResource, CommerceLayerClient, Resource } from "@commercelayer/sdk"
-import { CommerceLayerBundle } from "@commercelayer/sdk/bundle"
+import type { CommerceLayerBundle } from "@commercelayer/sdk/bundle"
 
 
 class CommerceLayerUtilsConfig {
@@ -22,18 +21,8 @@ class CommerceLayerUtilsConfig {
 const clUtilsConfig = new CommerceLayerUtilsConfig()
 
 
-// type CommerceLayerLegacy = CommerceLayerClient & { [k in keyof CommerceLayerBundle ]: ApiResource<Resource>}
-
-
-function sdkClientCheck(cl: CommerceLayerBundle): boolean {
-	if (!cl) throw new Error('Invalid Commerce Layer client provided: undefined')
-	if (!cl.application) throw new Error('Invalid Commerce Layer client provided: only sdk 6 client and sdk 7 bundle client can be used with this sdk-utils')
-	return true
-}
-
-
 const CommerceLayerUtils = (cl?: CommerceLayerBundle): CommerceLayerUtilsConfig => {
-	if (cl && sdkClientCheck(cl)) clUtilsConfig.sdk = cl
+	if (cl) clUtilsConfig.sdk = cl
 	return clUtilsConfig
 }
 

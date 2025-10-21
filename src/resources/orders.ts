f69e30011f3341e8d2e3e55c680268f20d4aaf32
Pierluigi Viti <pierluigi@commercelayer.io>
2025-10-21 16:08:45 +0200

    fix: check order for free skus

--- src/resources/orders.ts
+++ src/resources/orders.ts
@@ -1,12 +1,16 @@
-import { Sku } from "@commercelayer/sdk"
+import { type Orders, type Sku } from "@commercelayer/sdk"
+import CommerceLayerUtils from "../init"
 
 
 
-export const availableFreeSkus = async (): Promise<Sku[]> => {
+export const availableFreeSkus = async (orderId: string): Promise<Sku[]> => {
 
   const freeSkus: Sku[] = []
 
+  const order = await CommerceLayerUtils().api<Orders>('orders').retrieve(orderId, { include: ['available_free_skus'], fields: ['available_free_skus'] })
 
+  if (order.available_free_skus?.length) freeSkus.push(...order.available_free_skus)
+  
   return freeSkus
 
 }
